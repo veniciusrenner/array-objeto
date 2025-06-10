88330c717af9a02c02302eb4fec565e3f026b5e0
VENICIUS RENNER SOUSA BARRETO <veniciusbarreto0755@digitalcollege.internal>
2025-06-10 20:18:22 -0300

    subindo projeto

---
+++ array.js
@@ -0,0 +1,32 @@
+// lista de itens doa comnetarios
+// 1. criar
+let nomes = ["Ana", "Bia", "Carlos", "Daniel", "Eduardo"];
+let carros = []
+
+const idades = [18, 20, 25, 30, 35];
+const cidadesArgentinas = [
+    "Buenos Aires", 
+    "Córdoba", 
+    "Rosario", 
+    "Mendoza", 
+    "La Plata"
+]
+
+//console.log(nomes)
+//nomes.pop() // remove o ultimo elemento
+//console.log(nomes);
+////console.log(nomes);
+//nomes.shift() // remove o primeiro elemento
+//console.log(nomes);
+//console.log(nomes[1])
+//console.log(`${nomes[1]} ${nomes[2]} `)
+
+//// 5. buscar elementos (filtrar)
+//// 6. percorrer
+
+let frutas = ['maçã', "banana", "laranja", "uva", "pera"];
+
+frutas.forEach(function (fruta){
+    console.log(fruta)
+
+})
