d2e7117a8fd07c9b8bbabe56ebe028311e19d47b
VENICIUS RENNER SOUSA BARRETO <veniciusbarreto0755@digitalcollege.internal>
2025-06-10 21:17:22 -0300

    subindo

---
+++ objeto.js
@@ -0,0 +1,31 @@
+let pessoa = {
+    nome: 'joão',
+    cpf: '12345678901',
+    genero: 'F',
+    idade: 30,
+    altura: 1.75,
+    dataNacismento: '1993-05-15',
+    endereco: 'Rua das Flores, 123',
+    adicionar: function() {
+        console.log('Adicionando pessoa...');  
+    },
+    remover: function() {
+        console.log('Removendo pessoa...');  
+    },
+    telefone: {
+        celular: '11987654321',
+        email: 'ana@gmail.com',
+        tamanhos: ['G', 'GG'],
+
+        estrangeiro: false
+    }
+  
+}
+console.log(pessoa);
+
+// Acessando propriedades do objeto
+console.log(pessoa.dataNacismento);
+console.log(pessoa.endereco);
+console.log(pessoa.telefone);
+
+
